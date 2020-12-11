54d865756c11b46573eb0c329bb0b097e25f85e8
FdogMain <2506897252@qq.com>
2020-12-11 19:32:55 +0800

    第二次提交

--- fdogtest.cpp
+++ fdogtest.cpp
@@ -1,6 +1,23 @@
+/*输入三个整数，分别存在变量x,y,z中。编程输出最大数。*/ 
 #include<stdio.h>
-int main()
+int main(void)
 {
-    printf("Fdog测试");
-    return 0;
+	int i;
+	int j[3];
+	int max;
+	max=j[0];
+	for(i=0;i<=2;i++)
+	{ 
+	scanf("%d",&j[i]); 
+	} 
+	for(i=0;i<=2;i++)
+	{
+		if(max<j[i])
+		{ 
+			max=j[i];
+		}
+	} 
+		printf("%d",max);
+	return 0;
 }
+
